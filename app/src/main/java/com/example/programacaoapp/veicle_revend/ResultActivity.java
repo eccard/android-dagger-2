081688bbadbdba8442869f24b7e39d7df2fc1e1c
Enieber Cunha <eniebercunha@gmail.com>
2016-12-02 17:53:52 -0200

    finished notification

--- app/src/main/java/com/example/programacaoapp/veicle_revend/ResultActivity.java
+++ app/src/main/java/com/example/programacaoapp/veicle_revend/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.example.programacaoapp.veicle_revend;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.cancelAll();
     }
 }
