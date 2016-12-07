00ee1ba7fa27c25827a4c803839951450b4b60b7
Enieber Cunha <eniebercunha@gmail.com>
2016-12-07 10:26:58 -0200

    add permitions and refactoring code

--- app/src/main/java/com/example/programacaoapp/veicle_revend/service/MyPrinter.java
+++
@@ -1,7 +0,0 @@
-package com.example.programacaoapp.veicle_revend.service;
-
-public class MyPrinter {
-    public void print(String s) {
-        System.out.println(s);
-    }
-}
--- app/src/main/java/com/example/programacaoapp/veicle_revend/service/RestService.java
+++
@@ -1,7 +0,0 @@
-package com.example.programacaoapp.veicle_revend.service;
-
-public class RestService {
-    public String getSomething() {
-        return "Hello world";
-    }
-}
