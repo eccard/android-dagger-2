7aa60a064262e52f2a0d2b3ac4689287da596316
Enieber Cunha <eniebercunha@gmail.com>
2016-12-08 10:57:10 -0200

    add locationListener in Service

--- app/src/main/java/com/example/programacaoapp/veicle_revend/LocationGps.java
+++ app/src/main/java/com/example/programacaoapp/veicle_revend/LocationGps.java
@@ -1,13 +1,55 @@
 package com.example.programacaoapp.veicle_revend;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 public class LocationGps extends Service {
 
     private static String TAG = "Inchoo.net tutorial";
+    private Context mContext;
+    private LocationManager mLocationManager;
+    Location location;
+    private double latitude;
+    private double longitude;
+    public boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    boolean canGetLocation = false;
+    private static final long LOCATION_REFRESH_DISTANCE = 1; // 10 meters
+    private static final long LOCATION_REFRESH_TIME = 1; // 1
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+        location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    }
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -16,10 +58,11 @@ public class LocationGps extends Service {
     }
 
     @Override
-    public void onStart(Intent intent, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId) {
         // TODO Auto-generated method stub
         super.onStart(intent, startId);
         Log.d(TAG, "FirstService started");
+        return flags;
     }
 
     @Override
@@ -27,5 +70,35 @@ public class LocationGps extends Service {
         // TODO Auto-generated method stub
         super.onDestroy();
         Log.d(TAG, "FirstService destroyed");
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            //your code here
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
     }
 }
