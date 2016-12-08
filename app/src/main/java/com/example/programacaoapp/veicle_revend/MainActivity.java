7afd0ae99c38674dcb00b4eda3c3a8167501c847
Enieber Cunha <eniebercunha@gmail.com>
2016-12-08 12:04:44 -0200

    get value of Service

--- app/src/main/java/com/example/programacaoapp/veicle_revend/MainActivity.java
+++ app/src/main/java/com/example/programacaoapp/veicle_revend/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.programacaoapp.veicle_revend;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.example.programacaoapp.veicle_revend.component.DaggerVehicleComponent;
@@ -24,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     Vehicle vehicle;
     LocationGps locationGps;
     TextView speedValue;
+    LocationGps mService;
+    boolean mBound = false;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +42,19 @@ public class MainActivity extends AppCompatActivity {
 
         speedValue = (TextView) findViewById(R.id.current_speed_value);
         startService(new Intent(this, LocationGps.class));
-//        if (locationGps.canGetLocation) {
-//            double latitude = locationGps.getLatitude();
-//            double longitude = locationGps.getLongitude();
-//
-//        Toast.makeText(getApplicationContext(), "Your location is -\nLat: "+ latitude +" -\nLong: "+longitude, Toast.LENGTH_LONG);
+        locationGps = new LocationGps();
 
         VehicleComponent component = DaggerVehicleComponent.builder().vehicleModule(new VehicleModule()).build();
         vehicle = component.provideVehicle();
 
         speedValue.setText(String.valueOf(vehicle.getSpeed()));
+        latitude = locationGps.getLatitude();
+        longitude = locationGps.getLongitude();
+    }
+
+    public void showMesage() {
+        Toast.makeText(this, "Your location is -\nLat: " + latitude + " -\nLong: " + longitude, Toast.LENGTH_LONG);
+        Log.v("location ", "Your location is -\nLat: " + latitude + " -\nLong: " + longitude); 
     }
 
     public void callBrake(View v){
@@ -75,14 +87,40 @@ public class MainActivity extends AppCompatActivity {
         mNotificationManager.notify(0, mBuilder.build());
         vehicle.stop();
         speedValue.setText(String.valueOf(vehicle.getSpeed()));
+
+        showMesage();
     }
 
     public void callIncrease(View v){
+        Intent intent = new Intent(this, LocationGps.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        if (mService != null) {
+            latitude = mService.getLatitude();
+            longitude = mService.getLongitude();
+        }
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 // mId allows you to update the notification later on.
         mNotificationManager.cancelAll();
         vehicle.increaseSpeed(10);
         speedValue.setText(String.valueOf(vehicle.getSpeed()));
+        showMesage();
     }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocationGps.LocalBinder binder = (LocationGps.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
 }
